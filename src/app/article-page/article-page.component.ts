26ceafc2ffbf22fb8ff2bb0c9406d2a06e76b513
Nicolas Leroux <hertford.fr@gmail.com>
2024-09-30 11:01:41 +0200

    feat: end of quest

--- src/app/article-page/article-page.component.ts
+++ src/app/article-page/article-page.component.ts
@@ -1,21 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { Article } from '../models/article.type';
-import { ActivatedRoute, ParamMap, RouterLink } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router, RouterLink } from '@angular/router';
+import { ArticleService } from '../services/article.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-article-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './article-page.component.html',
   styleUrl: './article-page.component.scss'
 })
 export class ArticlePageComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  articleId!: number
+  router: Router = inject(Router);
+  articleService: ArticleService = inject(ArticleService);
+  articleId!: number;
+  article!: Article;
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.articleId = Number(params.get('id'));
     });
+    this.article = this.articleService.getArticleById(this.articleId);
+    if (this.article === undefined) {
+      this.router.navigate(['/not-found']);
+    }
   }
 }
